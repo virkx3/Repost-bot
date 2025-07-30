7dc9e8a248f54d343b2ed1d97bcc6b61cfd05ce5
Virk <40689073+virkx3@users.noreply.github.com>
2025-07-30 13:25:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const axios = require("axios");
 const ffmpeg = require("fluent-ffmpeg");
 const ffmpegPath = require("ffmpeg-static");
 const path = require("path");
-const { Octokit } = require("@octokit/rest");
 
 puppeteer.use(StealthPlugin());
 ffmpeg.setFfmpegPath(ffmpegPath);
