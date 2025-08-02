acb928609dd99d65392b421f3dcb1d2972c438e6
Virk <40689073+virkx3@users.noreply.github.com>
2025-08-02 15:44:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function addCaptionOverlayAndTransform(inputPath, outputPath, caption) {
           filter: 'drawtext',
           options: {
             text: caption,
-            fontfile: '/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf',
+            fontfile: '/usr/share/fonts/truetype/noto/NotoSansDevanagari-Regular.ttf'
             fontsize: 36,
             fontcolor: 'white',
             x: '(w-text_w)/2',
